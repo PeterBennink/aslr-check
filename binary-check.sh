36ea56b9c774d2ffddd2b5d96d6a0128f9072bd0
root <root@derby.studlab.os3.nl>
2018-04-26 16:04:15 +0200

    date added

--- binary-check.sh
+++ binary-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DATE=$(date +%m-%d-%Y_%H:%M:%S)
+
 for i in `sed 's/:/\n/g' <<< "$PATH"`
 do
-	find $i -exec file {} \; | grep -i elf | sed 's/:.*$//' | xargs hardening-check > pie-output
+	find $i -exec file {} \; | grep -i elf | sed 's/:.*$//' | xargs hardening-check >> pie-output-$DATE
 done
