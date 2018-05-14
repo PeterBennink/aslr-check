60e332f9d3dd690d5d957e56e156b61b5f11b214
PeterBennink <peter.bennink@gmail.com>
2018-05-14 11:14:21 +0200

    Update binary-check.sh
    echo 'randomize_va_space' && cat /proc/sys/kernel/randomize_va_space

--- binary-check.sh
+++ binary-check.sh
@@ -6,3 +6,5 @@ for i in `sed 's/:/\n/g' <<< "$PATH"`
 do
 	find $i -exec file {} \; | grep -i elf | sed 's/:.*$//' | xargs hardening-check >> pie-$DATE.log
 done
+
+echo 'randomize_va_space' && cat /proc/sys/kernel/randomize_va_space
